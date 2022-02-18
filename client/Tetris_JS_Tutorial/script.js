6ce0eb9d7672056f7661f71047dd8e9cb5885eb6
JoeyPoling27 <98398920+JoeyPoling27@users.noreply.github.com>
2022-02-18 15:54:27 -0500

    moveDown and freeze function added

--- client/Tetris_JS_Tutorial/script.js
+++ client/Tetris_JS_Tutorial/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid');
-  const squares = Array.from(document.querySelectorAll('.grid div'));
-  const scoreDisplay = document.querySelector('#score');
-  const startButton = document.querySelector('#startButton');
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const squares = Array.from(document.querySelectorAll(".grid div"));
+  const scoreDisplay = document.querySelector("#score");
+  const startButton = document.querySelector("#startButton");
   const width = 10;
 
   // Tetrominoes
@@ -10,39 +10,45 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2]
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
   ];
 
   const zTetromino = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1]
+    [width + 1, width + 2, width * 2, width * 2 + 1],
   ];
 
   const tTetromino = [
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1]
+    [1, width, width + 1, width * 2 + 1],
   ];
 
   const oTetromino = [
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
-    [0, 1, width, width + 1]
+    [0, 1, width, width + 1],
   ];
 
   const iTetromino = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
     [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3]
+    [width, width + 1, width + 2, width + 3],
   ];
 
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino,
-    tTetromino, oTetromino, iTetromino];
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
 
   let currentPosition = 4;
   let currentRotation = 0;
@@ -54,14 +60,38 @@ document.addEventListener('DOMContentLoaded', () => {
   // Draw tetrominoes
   function draw() {
     current.forEach((index) => {
-      squares[currentPosition + index].classList.add('tetromino');
+      squares[currentPosition + index].classList.add("tetromino");
     });
   }
 
   // Undraw tetrominoes
   function undraw() {
     current.forEach((index) => {
-        squares[currentPosition + index].classList.remove('tetromino');
+      squares[currentPosition + index].classList.remove("tetromino");
     });
+  }
+
+  // Make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000);
+
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
+
+  // freeze function
+  function freeze() {
+    if (
+      current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))
+    ) {
+      current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
+      // Start new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
   }
 });
