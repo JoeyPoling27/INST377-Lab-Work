9824eada3e388cd3898553722af4eccad90f27c4
JoeyPoling27 <98398920+JoeyPoling27@users.noreply.github.com>
2022-03-13 21:52:18 -0400

    Lab 6 - Restaurant display update

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -6,12 +6,28 @@ function getRandomIntInclusive(min, max) {
     ); //The maximum is inclusive and the minimum is inclusive
 }
 
-function dataHandler(dataArray) {
+function restoArrayMake(dataArray) {
   console.log('fired dataHandler');
-  console.table(dataArray); // this is called "dot notation"
+  // console.table(dataArray); // this is called "dot notation"
   const range = [...Array(15).keys()];
-  range.forEach((item) => {
-    console.log('range item', item);
+  const listItems = range.map((item, index) => {
+    const resNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[resNum];
+  });
+
+  // console.log(listItems);
+  return listItems;
+}
+
+function createHtmlList(collection){
+  console.log(collection);
+  const targetList = document.querySelector('.resto-list')
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${item.name}</li>`;
+    targetList.innerHTML += injectThisItem;
 
   });
 }
@@ -23,14 +39,16 @@ async function mainEvent() { // the async keyword means we can make API requests
   submit.style.display = 'none';
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
   if(arrayFromJson.data.length > 0){
+    submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
-      submit.style.display = 'block';
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      dataHandler(arrayFromJson.data);
+      const restoArray = restoArrayMake(arrayFromJson.data);
+      createHtmlList(restoArray);
     });
   }
 }
