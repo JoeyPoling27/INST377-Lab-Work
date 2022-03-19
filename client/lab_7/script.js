5174b0a8d99b09647801bfd9d53a88330637eed6
JoeyPoling27 <98398920+JoeyPoling27@users.noreply.github.com>
2022-03-19 18:30:38 -0400

    Lab 7 finishing touches
    Sort by name and city are implemented

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -7,7 +7,7 @@ function getRandomIntInclusive(min, max) {
   }
   
   function restoArrayMake(dataArray) {
-    console.log('fired dataHandler');
+    // console.log('fired dataHandler');
     // console.table(dataArray); // this is called "dot notation"
     const range = [...Array(15).keys()];
     const listItems = range.map((item, index) => {
@@ -25,10 +25,11 @@ function getRandomIntInclusive(min, max) {
     targetList.innerHTML = '';
     collection.forEach((item) => {
       const {name} = item;
-      const displayName = name.toLowerCase();
-      const injectThisItem = `<li>${item.name}</li>`;
-      targetList.innerHTML += injectThisItem;
-  
+      const displayName = 'Restaurant Name: ' + name.toLowerCase();
+      const displayCity = 'City: ' + item.city.toLowerCase();
+      const injectThisItem = `<li>${displayName}</li>`;
+      const injectThisItemCity = `<li>${displayCity}</li>`;
+      targetList.innerHTML += injectThisItem + injectThisItemCity + `</br>`;
     });
   }
   
@@ -36,19 +37,56 @@ function getRandomIntInclusive(min, max) {
     console.log('script loaded');
     const form = document.querySelector('.main_form');
     const submit = document.querySelector('.submit_button')
+    const resto = document.querySelector('#resto_name');
+    const restoCity = document.querySelector('#resto_city');
     submit.style.display = 'none';
+
     const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.log(arrayFromJson);
+    // console.log(arrayFromJson);
+
+    // Prevent race condition on data load
     if(arrayFromJson.data.length > 0){
       submit.style.display = 'block';
+
+      //Sort by name
+      let currentRestoArray = [];
+      resto.addEventListener('input', async(event) => {
+        if (currentRestoArray.length < 1) {
+          return;
+        }
+        
+        // console.log(event.target.value);
+        const selectResto = currentRestoArray.filter((resto) => {
+          const lowerName = resto.name.toLowerCase();
+          const lowerValue = event.target.value.toLowerCase();
+          return lowerName.includes(lowerValue);
+        });
+        createHtmlList(selectResto);
+      });
+
+      //Sort by city
+      restoCity.addEventListener('input', async(event) => {
+        if (currentRestoArray.length < 1) {
+          return;
+        }
+        // console.log(event.target.value);
+        const selectRestoCity = currentRestoArray.filter((restoCity) => {
+          const lowerName = restoCity.city.toLowerCase();
+          const lowerValue = event.target.value.toLowerCase();
+          return lowerName.includes(lowerValue);
+        });
+        createHtmlList(selectRestoCity);
+      });
+
       form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
         submitEvent.preventDefault(); // This prevents your page from refreshing!
-        console.log('form submission'); // this is substituting for a "breakpoint"
+        // console.log('form submission'); // this is substituting for a "breakpoint"
         // arrayFromJson.data - we're accessing a key called 'data' on the returned object
         // it contains all 1,000 records we need
-        const restoArray = restoArrayMake(arrayFromJson.data);
-        createHtmlList(restoArray);
+        currentRestoArray = restoArrayMake(arrayFromJson.data);
+        createHtmlList(currentRestoArray);
+        
       });
     }
   }
